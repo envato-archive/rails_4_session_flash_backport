0ea7cc407cd5fba98e6eeadff581dab8a21c66ea
Lucas Parry <lucas@envato.com>
2012-12-14 11:48:22 +1100

    Reorganise files

--- lib/rails_4_session_flash_backport.rb
+++ lib/rails_4_session_flash_backport.rb
@@ -3,9 +3,10 @@ require "rails_4_session_flash_backport/version"
 
 case Rails.version.to_i
 when 2
-  require 'rails_4_session_flash_backport/rails2'
+  require 'rails_4_session_flash_backport/rails2/flash_hash'
+  require 'rails_4_session_flash_backport/rails2/session_with_indifferent_access'
 when 3
-  require 'rails_4_session_flash_backport/rails3'
+  require 'rails_4_session_flash_backport/rails3/flash_hash'
 else
   Rails.logger.warn "rails_4_session_flash_backport doesnt yet do anything on Rails #{Rails.version}"
 end
--- spec/rails2/flash_hash_spec.rb
+++ spec/rails2/flash_hash_spec.rb
@@ -1,19 +1,24 @@
-require 'action_dispatch'
-require 'rails_4_session_flash_backport/rails3'
+require 'action_controller'
+require 'rails_4_session_flash_backport/rails2/flash_hash'
 
-describe ActionDispatch::Flash::FlashHash, "hax" do
+describe ActionController::Flash::FlashHash, "hax" do
 
   let(:rails_2_marshaled) { "\x04\bIC:'ActionController::Flash::FlashHash{\a:\vnoticeI\"\x11I'm a notice\x06:\x06ET:\nerrorI\"\x11I'm an error\x06;\aT\x06:\n@used{\a;\x06T;\bF" }
   let(:rails_3_marshaled) { "\x04\bo:%ActionDispatch::Flash::FlashHash\t:\n@usedo:\bSet\x06:\n@hash{\x06:\vnoticeT:\f@closedF:\r@flashes{\a;\tI\"\x11I'm a notice\x06:\x06EF:\nerrorI\"\x11I'm an error\x06;\fF:\t@now0" }
   let(:rails_2_vanilla)   { Marshal.load(rails_2_marshaled) }
   let(:rails_3_vanilla)   { Marshal.load(rails_3_marshaled) }
   let(:rails_4_style)     { {'flashes' => {:notice => "I'm a notice", :error => "I'm an error"}, 'discard' => [:notice]} }
-  let(:rails_2_marshaled) { "\x04\bIC:'ActionController::Flash::FlashHash{\a:\vnoticeI\"\x11I'm a notice\x06:\x06ET:\nerrorI\"\x11I'm an error\x06;\aT\x06:\n@used{\a;\x06T;\bF" }
-  let(:rails_2_vanilla) { Marshal.load(rails_2_marshaled) }
 
-  it "happily unmarshals a Rails 2 session without exploding" do
-    Marshal.load(rails_2_marshaled).should be_a(ActionController::Flash::FlashHash)
-  end
+   it "happily unmarshals a Rails 3 session without exploding" do
+     Marshal.load(rails_3_marshaled).should be_a(ActionDispatch::Flash::FlashHash)
+   end
+
+   context "#to_session_value" do
+     it "dumps to basic objects like rails 4" do
+       rails_2_vanilla.to_session_value.should be_a(Hash)
+       rails_2_vanilla.to_session_value.should == rails_4_style
+     end
+   end
 
   context "#from_session_value" do
     def this_is_the_flash_hash_were_looking_for(flash_hash)
@@ -26,23 +31,15 @@ describe ActionDispatch::Flash::FlashHash, "hax" do
     end
 
     it "decodes rails 2 style to an empty FlashHash" do
-      this_is_the_flash_hash_were_looking_for(ActionDispatch::Flash::FlashHash.from_session_value(rails_2_vanilla))
+      this_is_the_flash_hash_were_looking_for(ActionController::Flash::FlashHash.from_session_value(rails_2_vanilla))
     end
 
     it "decodes rails 3 style to a FlashHash" do
-      this_is_the_flash_hash_were_looking_for(ActionDispatch::Flash::FlashHash.from_session_value(rails_3_vanilla))
+      this_is_the_flash_hash_were_looking_for(ActionController::Flash::FlashHash.from_session_value(rails_3_vanilla))
     end
 
     it "decodes rails 4 style to a FlashHash" do
-      this_is_the_flash_hash_were_looking_for(ActionDispatch::Flash::FlashHash.from_session_value(rails_4_style))
+      this_is_the_flash_hash_were_looking_for(ActionController::Flash::FlashHash.from_session_value(rails_4_style))
     end
   end
-
-  context "#to_session_value" do
-    it "dumps to basic objects like rails 4" do
-      rails_3_vanilla.to_session_value.should be_a(Hash)
-      rails_3_vanilla.to_session_value.should == rails_4_style
-    end
-  end
-
 end
