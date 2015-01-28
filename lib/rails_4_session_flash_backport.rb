56aaf1e6e49493818dd4fcba8c59f1c3818eb97f
David Goodlad <david@goodlad.net>
2015-01-29 09:13:23 +1100

    Remove (broken) warning about rails 4

--- lib/rails_4_session_flash_backport.rb
+++ lib/rails_4_session_flash_backport.rb
@@ -7,8 +7,6 @@ when 2
   require 'rails_4_session_flash_backport/rails2/session_with_indifferent_access'
 when 3
   require 'rails_4_session_flash_backport/rails3/flash_hash'
-when 4
-  Rails.logger.warn "You're on Rails 4 so it's probably safe to remove the rails_4_session_flash_backport gem!"
 else
   Rails.logger.warn "rails_4_session_flash_backport doesnt yet do anything on Rails #{Rails.version}"
 end
