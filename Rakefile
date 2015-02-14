a638d6e541fe7bc63eb7186f35165cc1f102d112
Samuel Cochran <sj26@sj26.com>
2015-02-14 22:14:11 +1100

    Separate implementation for rails 3.0

--- Rakefile
+++ Rakefile
@@ -1,11 +1,26 @@
-#!/usr/bin/env rake
 require 'rubygems'
 require 'bundler/setup'
 require 'bundler/gem_tasks'
 
+desc "Run specs for loaded version of rails"
 task :spec do
-  rails_version = ENV["BUNDLE_GEMFILE"][/rails\-(\d)/, 1]
-  system "bundle", "exec", "rspec", "--color", "spec/rails#{rails_version}" or fail
+  rails_version = Gem.loaded_specs["rails"].version.to_s
+
+  spec_dir =
+    case rails_version
+    when /\A2\./
+      "spec/rails2"
+    when /\A3\.0\./
+      "spec/rails3-0"
+    when /\A3\./
+      "spec/rails3-1"
+    when /\A4\./
+      "spec/rails4"
+    else
+      fail "rails_4_session_flash_backport doesnt yet do anything on Rails #{rails_version}"
+    end
+
+  system "bundle", "exec", "rspec", "--color", spec_dir or fail
 end
 
 task :default => :spec
---
+++ spec/rails3-1/flash_hash_spec.rb
@@ -0,0 +1,62 @@
+require 'base64'
+
+require 'rails_4_session_flash_backport/rails3-1/flash_hash'
+
+describe ActionDispatch::Flash::FlashHash, "backport" do
+  context "#from_session_value" do
+    subject(:flash) { described_class.from_session_value(value) }
+
+    context "with rails 4 style session value" do
+      context "without discards" do
+        let(:value) { {"flashes" => {"greeting" => "Hello"}} }
+
+        it "is the expected flash" do
+          expect(flash.to_hash).to eq("greeting" => "Hello")
+        end
+      end
+
+      context "with discards" do
+        let(:value) { {"flashes" => {"greeting" => "Hello", "farewell" => "Goodbye"}, "discard" => ["farewell"]} }
+
+        it "is the expected flash" do
+          expect(flash.to_hash).to eq("greeting" => "Hello")
+        end
+      end
+    end
+
+    context "with rails 3 style session value" do
+      # {"session_id"=>"f8e1b8152ba7609c28bbb17ec9263ba7", "flash"=>#<ActionDispatch::Flash::FlashHash:0x00000000000000 @used=#<Set: {"farewell"}>, @closed=false, @flashes={"greeting"=>"Hello", "farewell"=>"Goodbye"}, @now=nil>}
+      let(:cookie) { 'BAh7B0kiD3Nlc3Npb25faWQGOgZFRkkiJWY4ZTFiODE1MmJhNzYwOWMyOGJiYjE3ZWM5MjYzYmE3BjsAVEkiCmZsYXNoBjsARm86JUFjdGlvbkRpc3BhdGNoOjpGbGFzaDo6Rmxhc2hIYXNoCToKQHVzZWRvOghTZXQGOgpAaGFzaHsGSSINZmFyZXdlbGwGOwBUVDoMQGNsb3NlZEY6DUBmbGFzaGVzewdJIg1ncmVldGluZwY7AFRJIgpIZWxsbwY7AFRJIg1mYXJld2VsbAY7AFRJIgxHb29kYnllBjsAVDoJQG5vdzA=' }
+      let(:session) { Marshal.load(Base64.decode64(cookie)) }
+      let(:value) { session["flash"] }
+
+      it "is the expected flash" do
+        expect(flash.to_hash).to eq("greeting" => "Hello")
+      end
+    end
+
+    context "with rails 2 style session value" do
+      # {"session_id"=>"f8e1b8152ba7609c28bbb17ec9263ba7", "flash"=>#<ActionController::Flash::FlashHash:0x00000000000000 @used={"farewell"=>true}, {"greeting"=>"Hello", "farewell"=>"Goodbye"}>}
+      let(:cookie) { 'BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJWY4ZTFiODE1MmJhNzYwOWMyOGJiYjE3ZWM5MjYzYmE3BjsAVEkiCmZsYXNoBjsAVElDOidBY3Rpb25Db250cm9sbGVyOjpGbGFzaDo6Rmxhc2hIYXNoewdJIg1mYXJld2VsbAY7AFRJIgxHb29kYnllBjsAVEkiDWdyZWV0aW5nBjsAVEkiCkhlbGxvBjsAVAY6CkB1c2VkewZJIg1mYXJld2VsbAY7AFRU' }
+      let(:session) { Marshal.load(Base64.decode64(cookie)) }
+      let(:value) { session["flash"] }
+
+      it "is the expected flash" do
+        expect(flash.to_hash).to eq("greeting" => "Hello")
+      end
+    end
+  end
+
+  context "#to_session_value" do
+    subject(:flash) { described_class.new }
+
+    before do
+      flash["greeting"] = "Hello"
+      flash.now["farewell"] = "Goodbye"
+    end
+
+    it "dumps to basic objects like rails 4" do
+      expect(flash.to_session_value).to eq("flashes" => {"greeting" => "Hello"})
+    end
+  end
+end
